578d9dbca83290b687e7bdb8f775eef6603601bf
Nolan Marketti <nolan@enemcreative.com>
2018-02-03 02:53:08 -0600

    Update Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,18 @@
-// webpack.config.js
 var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-var CompressionPlugin = require("compression-webpack-plugin");
-const path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: {
         site: "./source/javascripts/site.js",
+        slicklightbox: "./source/javascripts/slick-lightbox.min.js",
         vendor: [
-                "jquery",
-                "TweenMax"
-
-            ]
+            "jquery",
+            "gsap/TweenMax",
+        ]
     },
     output: {
-        filename: "javascripts/[name].js", //Would like to use [name].[chunkhash]
-        path: __dirname + '/.tmp/dist',
-    },
-    resolve: {
-        alias: {
-            "TweenMax": __dirname + '/node_modules/gsap/src/uncompressed/TweenMax',
-        }
+        filename: "javascripts/[name].js",
+        path: __dirname + '/.tmp/dist'
     },
     module: {
         rules: [
@@ -36,7 +26,7 @@ module.exports = {
                     ]
                 })
             },
-            {
+            { 
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
@@ -49,14 +39,6 @@ module.exports = {
         }),
         new webpack.optimize.CommonsChunkPlugin({
             name: "vendor"
-        }),
-
-        new UglifyJSPlugin({
-            compress: { warnings: false }
-        }),
-
-        //new BundleAnalyzerPlugin(),
-
-        //new CompressionPlugin({})
+        })
     ]
 }
